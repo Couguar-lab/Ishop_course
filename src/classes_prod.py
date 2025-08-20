a769a87a827442f6b25bcdd5d970078af9e27a01
Evg Fomin <Couguar72@gmail.com>
2025-08-20 20:39:03 +0300

    Выполнена настройка проекта

---
+++ src/classes_prod.py
@@ -0,0 +1,25 @@
+class Product:
+    name: str
+    description: str
+    price: float
+    quantity: int
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
+
+class Category:
+    name: str
+    description: str
+    products: list
+    category_count: int
+    product_count: int
+    def __init__(self, name, description, products):
+        self.name = name
+        self.description = description
+        self.products = products
+        self.category_count += 1
+        self.product_count += 1
+        # Category.category_count += 1
+        # Category.product_count += 1
